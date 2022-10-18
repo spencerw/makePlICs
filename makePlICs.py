5376ab8a4664768bcff93ef0d0470af4c30eaf91
Spencer Wallace <scw7@uw.edu>
2022-10-18 10:59:14 -0700

    Add feature to check for and resolve overlapping particles

--- makePlICs.py
+++ makePlICs.py
@@ -1,6 +1,7 @@
 import scipy.interpolate as interpolate
 import numpy as np
 import pynbody as pb
+import pandas as pd
 import os
 import sys
 
@@ -28,7 +29,7 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
     filledLength = int(length * iteration // total)
     bar = fill * filledLength + '-' * (length - filledLength)
-    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd, flush=True)
     # Print New Line on Complete
     if iteration == total: 
         print()
@@ -116,10 +117,15 @@ if 'gas_profile' not in params:
 else:
   gas_profile = int(params['gas_profile'])
 
+if 'inc_inf' not in params:
+    inc_inf = 1
+else:
+    inc_inf = params['inc_inf']
+
 use_bary = int(params['use_bary'])
 start_idx = use_bary + add_planet
 n_particles = int((params['m_disk']*u.M_earth).to(u.g).value/params['m_pl'])
-print('Build a disk with ' + str(n_particles) + ' planetesimals')
+print('Build a disk with ' + str(n_particles) + ' planetesimals', flush=True)
 ntotal = n_particles + use_bary + add_planet
 ndim = 3
 time = 0
@@ -179,7 +185,7 @@ else:
     ecc_std_val = eh_eq*(m_pl/(3*(m_central*u.M_sun).to(u.g).value))**(1/3)
     ecc_std = np.ones_like(a_vals)*ecc_std_val
 
-inc_std = ecc_std/2
+inc_std = ecc_std/2*inc_inf
 m_pl = (m_pl*u.g).to(u.M_sun).value
 r_pl = (r_pl*u.cm).to(u.AU).value
 
@@ -217,49 +223,84 @@ k_vals = np.empty(n_particles)
 p_vals = np.empty(n_particles)
 q_vals = np.empty(n_particles)
 
-print("Drawing orbital elements for particles...")
-printProgressBar(0, n_particles, prefix = 'Progress:', suffix = 'Complete', length = 50)
-for idx in range(n_particles):
-    printProgressBar(idx + 1, n_particles, prefix = 'Progress:', suffix = 'Complete', length = 50)
-    if checkOverlap:
-        isOverlap = True
-    else:
-        isOverlap = False
+def genPosVelForVec(iords, positions, velocities):
+    print("Drawing orbital elements for " +str(len(iords))+  " particles...", flush=True)
 
-    while isOverlap:
-        h_vals[idx] = np.random.normal(0, ecc_std[idx])
-        k_vals[idx] = np.random.normal(0, ecc_std[idx])
-        p_vals[idx] = np.random.normal(0, inc_std[idx])
-        q_vals[idx] = np.random.normal(0, inc_std[idx])
+    h_vals = np.random.normal(0, ecc_std[iords], len(iords))
+    k_vals = np.random.normal(0, ecc_std[iords], len(iords))
+    p_vals = np.random.normal(0, inc_std[iords], len(iords))
+    q_vals = np.random.normal(0, inc_std[iords], len(iords))
+
+    # Vectorize this?
+    if add_planet and sec_force:
+        print("Secular forcing broken for now, need to vectorize this! Exiting...")
+        exit()
+        #h_vals[idx] += e_forced(a_vals[idx])
+
+    inc_vals = np.sqrt(p_vals**2 + q_vals**2)
+    Omega_vals = np.arctan2(q_vals, p_vals)
+
+    ecc_vals = np.sqrt(h_vals**2 + k_vals**2)
+    varpi_vals = np.arctan2(k_vals, h_vals)
+    omega_vals = varpi_vals - Omega_vals
+    omega_vals = omega_vals%(2*np.pi)
+    M_vals = np.random.rand(len(iords))*2*np.pi
+
+    # Because of netwon-raphson iteration, kep2cart cant be vectorized
+    for idx in range(len(iords)):
+        if idx%1000 == 0:
+            print(str(idx) + ' of ' + str(len(iords)), flush=True)
+        p_x, p_y, p_z, v_x, v_y, v_z = ko.kep2cart(a_vals[iords[idx]], ecc_vals[idx], inc_vals[idx],\
+                                       Omega_vals[idx], omega_vals[idx], M_vals[idx], masses[iords[idx]], m_central)
+
+        positions[iords[idx]] = p_x, p_y, p_z
+        velocities[iords[idx]] = v_x, v_y, v_z
+
+    return positions, velocities
+
+def genPosVelFor(iords, positions, velocities):
+    print("Drawing orbital elements for " +str(len(iords))+  " particles...", flush=True)
+
+    #printProgressBar(0, n_particles, prefix = 'Progress:', suffix = 'Complete', length = 50)
+    for i in range(len(iords)):
+        if i%1000 == 0:
+            print(str(i) + ' of ' + str(len(iords)), flush=True)
+        #printProgressBar(i + 1, n_particles, prefix = 'Progress:', suffix = 'Complete', length = 50)
+        iord = iords[i]
+
+        h_val = np.random.normal(0, ecc_std[iord])
+        k_val = np.random.normal(0, ecc_std[iord])
+        p_val = np.random.normal(0, inc_std[iord])
+        q_val = np.random.normal(0, inc_std[iord])
         if add_planet and sec_force:
-            h_vals[idx] += e_forced(a_vals[idx])
+            h_val += e_forced(a_vals[iord])
 
-        inc_val = np.sqrt(p_vals[idx]**2 + q_vals[idx]**2)
-        Omega_val = np.arctan2(q_vals[idx], p_vals[idx])
+        inc_val = np.sqrt(p_val**2 + q_val**2)
+        Omega_val = np.arctan2(q_val, p_val)
 
-        ecc_val = np.sqrt(h_vals[idx]**2 + k_vals[idx]**2)
-        varpi_val = np.arctan2(k_vals[idx], h_vals[idx])
+        ecc_val = np.sqrt(h_val**2 + k_val**2)
+        varpi_val = np.arctan2(k_val, h_val)
         omega_val = varpi_val - Omega_val
         omega_val = omega_val%(2*np.pi)
         M_val = np.random.rand()*2*np.pi
 
-        p_x, p_y, p_z, v_x, v_y, v_z = ko.kep2cart(a_vals[idx], ecc_val, inc_val,\
-                                       Omega_val, omega_val, M_val, masses[idx], m_central)
+        p_x, p_y, p_z, v_x, v_y, v_z = ko.kep2cart(a_vals[iord], ecc_val, inc_val,\
+                                       Omega_val, omega_val, M_val, masses[iord], m_central)
 
-        # Make sure no overlap, otherwise redraw
-        for idx1 in range(0, idx):
-            x, y, z = positions[idx1+start_idx]
-            dist = np.sqrt((x - p_x)**2 + (y - p_y)**2 + (z - p_z)**2)
-            if dist < 2*r_pl:
-                isOverlap = True
-                break
-        
-        isOverlap = False
+        positions[iord] = p_x, p_y, p_z
+        velocities[iord] = v_x, v_y, v_z
 
-    positions[idx+start_idx] = p_x, p_y, p_z
-    velocities[idx+start_idx] = v_x, v_y, v_z
+    return positions, velocities
 
+# First time through, generate positions and velocities for all particles
+positions, velocities = genPosVelForVec(np.arange(start_idx, n_particles+start_idx), positions, velocities)
+
+# Warning: this is broken when doing overlap checks. Need to fix later!
+# Also, indices are probably incorrect when this is turned on
 if use_bary:
+    if checkOverlap:
+        print("Barycenteric correction not currently possible with overlap detection! Aborting...")
+        exit()
     masses[0] = m_central
     eps[0] = 1e-10
 
@@ -287,39 +328,94 @@ if use_bary:
 pot = np.zeros(ntotal)
 
 if fmt == 'tipsy':
-    f = open('ic.txt', 'w')
 
-    f.write(str(ntotal) + ', 0, 0\n')
-    f.write(str(ndim) + '\n')
-    f.write(str(time) + '\n')
-
-    for idx in range(ntotal):
-        f.write(str(masses[idx]) + '\n')
-
-    for idx in range(ntotal):
-        f.write(str(positions[:,0][idx]) + '\n')
-    for idx in range(ntotal):
-        f.write(str(positions[:,1][idx]) + '\n')
-    for idx in range(ntotal):
-        f.write(str(positions[:,2][idx]) + '\n')
+    locked_mask = np.zeros(n_particles)
     
-    for idx in range(ntotal):
-        f.write(str(velocities[:,0][idx]) + '\n')
-    for idx in range(ntotal):
-        f.write(str(velocities[:,1][idx]) + '\n')
-    for idx in range(ntotal):
-        f.write(str(velocities[:,2][idx]) + '\n')
-    
-    for idx in range(ntotal):
-        f.write(str(eps[idx]) + '\n')
-    
-    for idx in range(ntotal):
-        f.write(str(pot[idx]) + '\n')
+    while True:
+        f = open('ic.txt', 'w')
 
-    f.close()
+        f.write(str(ntotal) + ', 0, 0\n')
+        f.write(str(ndim) + '\n')
+        f.write(str(time) + '\n')
 
-    os.system(os.path.expanduser('~') + "/tipsy_tools/ascii2bin < ic.txt > " + filename)
-    os.system("rm ic.txt")
+        for idx in range(ntotal):
+            f.write(str(masses[idx]) + '\n')
+
+        for idx in range(ntotal):
+            f.write(str(positions[:,0][idx]) + '\n')
+        for idx in range(ntotal):
+            f.write(str(positions[:,1][idx]) + '\n')
+        for idx in range(ntotal):
+            f.write(str(positions[:,2][idx]) + '\n')
+        
+        for idx in range(ntotal):
+            f.write(str(velocities[:,0][idx]) + '\n')
+        for idx in range(ntotal):
+            f.write(str(velocities[:,1][idx]) + '\n')
+        for idx in range(ntotal):
+            f.write(str(velocities[:,2][idx]) + '\n')
+        
+        for idx in range(ntotal):
+            f.write(str(eps[idx]) + '\n')
+        
+        for idx in range(ntotal):
+            f.write(str(pot[idx]) + '\n')
+
+        f.close()
+
+        os.system(os.path.expanduser('~') + "/tipsy_tools/ascii2bin < ic.txt > output.ic")
+        os.system("rm ic.txt")
+
+        num_over = 0
+        if checkOverlap:
+            os.system("rm overlap.log")
+            os.system(os.path.expanduser('~') + "/changa_uw_collision_overlap/ChaNGa overlap.param > output.changa")
+
+            df = pd.read_csv('overlap.log', delimiter=' ', names=['i1', 'i2'])
+            print("Size of overlap log: " + str(len(df)))
+            max_iord = df.max(axis=1).values
+            print("# of unique max iords: " + str(len(np.unique(max_iord))))
+
+            ########
+            # Method 3 - select iords from column 1 that arent in column 2
+            # Even quicker than 2, but misses even more overlaps!
+            ########
+            #i1, i2 = df['i1'].values, df['i2'].values
+            #over_iord = i1[~np.isin(i1, i2)]
+
+            ########
+            # Method 2 - sort columns by iord before selecting from column 1
+            # Much quicker and more efficient, but this doesnt appear to
+            # resolve all of the overlaps
+            ########
+            #df_sort = pd.DataFrame(np.sort(df,axis=1),index=df.index)
+            #over_iord = np.unique(df_sort[1].values)
+
+            ########
+            # Method 1 - select unique iords from overlap list
+            # This does the job, but we are replacing way too many particles!
+            # Too slow for 1e6 satellitesimals
+            ########
+            #over_iord = np.unique(np.concatenate([df['i1'].values, df['i2'].values]))
+
+            over_iord = df['i1'].values
+
+            # Lock every particle that isnt in the overlap list
+            #not_over = np.argwhere(~np.isin(np.arange(0, n_particles), over_iord))[:,0]
+            #locked_mask[not_over] = 1
+
+            #num_over = len(locked_mask[locked_mask == 0])
+            num_over = len(over_iord)
+            print("Remaining overlaps: " + str(num_over), flush=True)
+            
+            if num_over > 0:
+                #positions, velocities = genPosVelForVec(np.argwhere(locked_mask == 0)[:,0], positions, velocities)
+                positions, velocities = genPosVelForVec(over_iord, positions, velocities)
+
+        if num_over == 0:
+            break
+
+    os.system("mv output.ic " + filename)
 
 elif fmt == 'genga':
     f = open(filename.replace("'", ""), 'w')
